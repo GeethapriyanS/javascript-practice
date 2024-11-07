986357efec5f793ce42144d4c5a58fed5598f6dc
GeethapriyanS <geethapriyan.s2023cse@sece.ac.in>
2024-11-07 22:32:31 +0530

    updated

--- introduction to java/javascript/script.js
+++ introduction to java/javascript/script.js
@@ -1,2 +1,42 @@
 console.log("Hello world");
 alert("Welcome to javascript");
+
+variable="mathematics";
+console.log(variable.length);
+
+
+// const myvar="24"
+// const mynumber=42
+// const myfloat=24.0
+
+// 1.console.log(mynumber===myfloat) // true
+// 2.console.log(Number(myvar)) // 24
+// 3.console.log(Number("hello")+3) // NaN
+
+// number methods
+
+// 4.console.log(Number.isInteger(434.2)) // false
+// 5.console.log(Number.parseFloat("423.099aa")) // 423.099
+// 6.onsole.log(Number.parseInt("423.099aa")) // 423
+// 7.console.log(Number.parseFloat("423.099aa").toFixed(2)) // "423.09" 
+// 8.console.log(myfloat.toSting());
+
+// chainning - combining methods using . operator 
+
+// 9.console.log(isNaN("Dhuddu")) // true
+// 10.console.log(Number.isNaN("Dhuddu")) // false
+
+
+// let --> we can change
+// const --> we can't change 
+
+// str="mathematics";
+// 1.str.length;
+// 2.str.charAt(0);
+// 3.str.indexOf('a');
+// 4.str.lastIndexOf('a');
+// 5.str.slice(start,end);
+// 6.str.toUpperCase();
+// 7.str.toLowerCase();
+// 8.str.includes("mat"); // return true
+// 9.str.split('e'); // ['math','matics']
