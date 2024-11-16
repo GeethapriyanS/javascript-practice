cb243567ceff6161e7c832116bb54fa84a9cc4e8
GeethapriyanS <geethapriyan.s2023cse@sece.ac.in>
2024-11-16 16:12:14 +0530

    javascript basics added

---
+++ javascript basics/javascript/script.js
@@ -0,0 +1,14 @@
+// Math Properties
+
+// 1.console.log(Math.PI);//3.1456789
+// 2.console.log(Math.trunc(Math.PI));//3
+// 3.console.log(Math.round(Math.PI));//3
+// 4.console.log(Math.round(4.5));//5
+// 5.console.log(Math.ceil(4.1));//5
+// 6.console.log(Math.floor(4.2));//4
+// 7.console.log(Math.trunc(Math.PI));
+// 8.console.log(Math.pow(4,3));//81
+// 9.console.log(Math.min(10,2,3,1));//1
+// 10.console.log(Math.max(10,2,3,1));//10
+// 11.console.log(Math.random());//0.00 to 0.99
+// 12.console.log(Math.floor(Math.random()*10)+1);//10
